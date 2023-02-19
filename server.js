c5e8761e8810a94e1563af8330ce4372f4c0e51f
Syed Ragib Israq <ragib.israq9833@gmail.com>
2023-02-19 12:51:48 +0600

    Database checking render

--- server.js
+++ server.js
@@ -13,11 +13,11 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-      host : 'dpg-cfo7s682i3mo4brs6580-a',
+      host : '127.0.0.1',
       port : 5432,
-      user : 'face_recogtion_app_user',
-      password : 'hUpaBEgEBmJ4pG9XY1O5d8Hlc8H60ZDu',
-      database : 'face_recogtion_app'
+      user : 'macbookpro',
+      password : '',
+      database : 'smart-brain'
     }
 });
 
