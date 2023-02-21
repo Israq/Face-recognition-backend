da3f82092e94d4a2d27d39c4def7cf2e8949409d
Syed Ragib Israq <ragib.israq9833@gmail.com>
2023-02-21 16:05:01 +0600

    Internal database url-5

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-      connectionString: "postgres://face_recogtion_app_zewr_user:KfKXjp3Uy8VJHqtXJktCY3CJIocmn70B@dpg-cfpidsqrrk0fd9tpo360-a/face_recogtion_app_zewr",
+      connectionString: process.env.DATABASE_URL,
       ssl: true,
 
 
