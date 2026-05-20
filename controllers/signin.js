8abf9f416142709c3268ef4205e17608c6932355
Israq <ragib.israq9833@gmail.com>
2026-05-20 23:18:14 +0600

    fix: update db ssl config and register controller

--- controllers/signin.js
+++ controllers/signin.js
@@ -1,25 +1,31 @@
 const handleSignin = (db, bcrypt) => (req, res) => {
-    const { email, password } = req.body;
-    if (!email ||!password ) {
-        return res.status(400).json('Incorrect Form Submission');
-    }
-    db.select('email', 'hash').from('login')
-        .where('email', '=', email)
-        .then(data => {
-           const isValid = bcrypt.compareSync(password , data[0].hash);
-            if (isValid) {
-             return db.select('*').from('users')
-             .where('email', '=', email)
-             .then(user => {
-                res.json(user[0])
-             })
-              .catch(err => res.status(400).json('Unable to Get User'))
-            } else {
-                res.status(400).json('Wrong Credentials')
-            }
-        })
-        .catch(err => res.status(400).json('Wrong credentials'))
-}
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(400).json("Incorrect Form Submission");
+  }
+  db.select("email", "hash")
+    .from("login")
+    .where("email", "=", email)
+    .then((data) => {
+      const isValid = bcrypt.compareSync(password, data[0].hash);
+      if (isValid) {
+        return db
+          .select("*")
+          .from("users")
+          .where("email", "=", email)
+          .then((user) => {
+            res.json(user[0]);
+          })
+          .catch((err) => {
+            console.log("Signin error:", err);
+            res.status(400).json("Wrong credentials");
+          });
+      } else {
+        res.status(400).json("Wrong Credentials");
+      }
+    })
+    .catch((err) => res.status(400).json("Wrong credentials"));
+};
 module.exports = {
-    handleSignin: handleSignin
+  handleSignin: handleSignin,
 };
--- server.js
+++ server.js
@@ -1,40 +1,48 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const bcrypt = require('bcrypt-nodejs');
-const cors = require('cors');
-const knex = require('knex');
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const bcrypt = require("bcrypt-nodejs");
+const cors = require("cors");
+const knex = require("knex");
 
-const register = require('./controllers/register');
-const signin = require('./controllers/signin');
-const profile = require('./controllers/profile')
-const image = require('./controllers/image');
+const register = require("./controllers/register");
+const signin = require("./controllers/signin");
+const profile = require("./controllers/profile");
+const image = require("./controllers/image");
 
- 
 const db = knex({
-    client: 'pg',
-    connection: {
-      connectionString: process.env.DATABASE_URL,
-      ssl: true,
-
-
-
-    }
+  client: "pg",
+  connection: {
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+      minVersion: "TLSv1.2",
+    },
+  },
 });
-
-
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/', (req,res) => { res.send('It is working') })
-app.post('/signin', signin.handleSignin( db, bcrypt))
-app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)})
-app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db)})    
-app.put('/image', (req, res) => { image.handleImage(req, res, db)})
-app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
+app.get("/", (req, res) => {
+  res.send("It is working");
+});
+app.post("/signin", signin.handleSignin(db, bcrypt));
+app.post("/register", (req, res) => {
+  register.handleRegister(req, res, db, bcrypt);
+});
+app.get("/profile/:id", (req, res) => {
+  profile.handleProfileGet(req, res, db);
+});
+app.put("/image", (req, res) => {
+  image.handleImage(req, res, db);
+});
+app.post("/imageurl", (req, res) => {
+  image.handleApiCall(req, res);
+});
 
 app.listen(process.env.PORT || 3000, () => {
-    console.log('app is running on port ${process.env.PORT}')
-})
+  console.log(`app is running on port ${process.env.PORT}`);
+});
